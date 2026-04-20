22d0b0662e4a70df1f7e2eaa6813438b2d3a410c
mezyene said <mezyenesaid23@gmail.com>
2026-04-20 23:18:43 +0200

    fix postgres connection

--- config/database.php
+++ config/database.php
@@ -1,12 +1,28 @@
 <?php
-$host = 'localhost';
-$db   = 'vite_gourmand3';
-$user = 'root';
-$pass = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    if (getenv("DATABASE_URL")) {
+        // 👉 HEROKU (PostgreSQL)
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        $host = $url["host"];
+        $user = $url["user"];
+        $pass = $url["pass"];
+        $db   = ltrim($url["path"], '/');
+
+        $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+    } else {
+        // 👉 LOCAL (MySQL)
+        $host = 'localhost';
+        $db   = 'vite_gourmand3';
+        $user = 'root';
+        $pass = '';
+
+        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    }
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Erreur DB : " . $e->getMessage());
 }
