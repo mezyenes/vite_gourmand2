8c14f95f66a33a593464fd9c3d0d50ade5e2d3a1
mezyene said <mezyenesaid23@gmail.com>
2026-04-22 12:24:18 +0200

    fix heroku db connection

--- config/database.php
+++ config/database.php
@@ -9,8 +9,19 @@ try {
         $user = $url["user"];
         $pass = $url["pass"];
         $db   = ltrim($url["path"], '/');
+        $port = $url["port"] ?? 5432;
 
-        $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+        $pdo = new PDO(
+            "pgsql:host=$host;port=$port;dbname=$db",
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]
+        );
+
     } else {
         // 👉 LOCAL (MySQL)
         $host = 'localhost';
@@ -19,6 +30,7 @@ try {
         $pass = '';
 
         $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
